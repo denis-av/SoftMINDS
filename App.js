54abf3717eba4c0951e7c22ace4ea1736b1c77ef
Diana Udisteanu <diana.udisteanu@yahoo.com>
2021-03-17 20:03:21 +0200

    Make connection with Firebase.

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>SoftMINDS</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const firebaseConfig = {
+  apiKey: "AIzaSyBs5q9LrmufF22x8DoYHsM2rvUHk1VBH84",
+  authDomain: "softminds-f4345.firebaseapp.com",
+  projectId: "softminds-f4345",
+  storageBucket: "softminds-f4345.appspot.com",
+  messagingSenderId: "364045688081",
+  appId: "1:364045688081:web:2fe5bd513c7c3c83ef40b0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      email: ""
+    };
+  }
+
+  addUserInDatabase = () => {
+    firebase.database().ref("user").set({
+      username: "diana",
+      password: "1234",
+      email: "diana.udisteanu@yahoo.com"
+    }).catch(function(error){
+      console.log(error.message)
+    })
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>SoftMINDS</Text>
+        <StatusBar style="auto" />
+        <Button title="Write" onPress = {this.addUserInDatabase}/> 
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +53,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
